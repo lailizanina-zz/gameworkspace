c9159511e0cb7be598353a1a20e9a87d388b4ad9
Victoria Nascimento Meneghel <victoria.nascimento@gmail.com>
2018-05-12 21:39:51 +0200

    created the enemy

--- ASCIIgame.py
+++ ASCIIgame.py
@@ -10,7 +10,7 @@ wn.title("Space Warriors")
 #Draw border
 border_pen = turtle.Turtle()
 border_pen.speed(0)
-border_pen.color('white')
+border_pen.color("white")
 border_pen.penup()
 border_pen.setposition(-300,-300)
 border_pen.pendown()
@@ -22,7 +22,7 @@ border_pen.hideturtle()
 
 #Create the player turtle
 player = turtle.Turtle()
-player.color("red")
+player.color("blue")
 player.shape("triangle")
 player.penup()
 player.speed(0)
@@ -31,24 +31,68 @@ player.setheading(90)
 
 playerspeed = 15
 
+#There is the enemy
+enemy = turtle.turtle()
+enemy.color("red")
+enemy.shape("circle")
+enemy.penup()
+enemy.speed(0)
+enemy.setupposition(-200, 250)
+
+enemyspeed = 2
+
+#Here is the bullet
+bullet = turtle.turtle
+bullet.color = ("yellow")
+bullet.shape = ("triangle")
+bullet.penup()
+bullet.speed(0)
+bullet.setheading(90)
+bullet.shapesize(0.5,0.5)
+bullet.hideturtle()
+
+bulletspeed = 20
+
 #Move the player left and right
 def move_left():
     x = player.xcor()
     x -= playerspeed
-    if x < -290:
-        x = -290
+    if x < -280:
+        x = -280
     player.setx(x)
 
 def move_right():
     x = player.xcor()
     x += playerspeed
-    if x < 290:
-        x = 290
+    if x < 280:
+        x = 280
     player.setx(x)
 
 #Create keyboard bindings
 turtle.listen()
 turtle.onkey(move_left, "Left")
 turtle.onkey(move_right, "Right")
+
+#main game loop
+while True:
+
+    #Moving the enemy
+    x = enemy.xcor()
+    x += enemyspeed
+    enemy.setx(x)
+
+    #move the enemy back and down
+    if enemy.xcor() > 280:
+        y = enemy.ycor
+        y -= 40
+        enemyspeed *= -1
+        enemy.set(y)
+        
+
+    if enemy.xcor() > -280:
+        y = enmy.ycor
+        y -= 40
+        enemyspeed *= -1
+        enemy.set(y)
 
 delay = input("Press enter to finish")
