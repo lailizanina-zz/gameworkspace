32fda9cc2e28b18e59ac36b90ee183d2f0bae445
Victoria Nascimento Meneghel <victoria.nascimento@gmail.com>
2018-07-03 14:24:21 +0200

    Now there is a game score working here!

--- ASCIIgame.py
+++ ASCIIgame.py
@@ -22,6 +22,19 @@ for side in range(4):
     border_pen.lt(90)
 border_pen.hideturtle()
 
+#Set the score to 0
+score = 0
+
+#Draw the score
+score_pen = turtle.Turtle()
+score_pen.speed(0)
+score_pen.color("white")
+score_pen.penup()
+score_pen.setposition(-290, 280)
+scorestring = "Score: %s" %score
+score_pen.write(scorestring, False, align="left", font=("Arial", 14, "normal"))
+score_pen.hideturtle()
+
 #Create the player turtle
 player = turtle.Turtle()
 player.color("blue")
@@ -150,6 +163,11 @@ while True:
             x = random.randint(-200, 200)
             y = random.randint(100, 250)
             enemy.setposition(x, y)
+            #Update the score
+            score += 10
+            scorestring = "Score: %s" %score
+            score_pen.clear()
+            score_pen.write(scorestring, False, align="left", font=("Arial", 14, "normal"))
 
         if isCollision(player, enemy):
             player.hideturtle()
